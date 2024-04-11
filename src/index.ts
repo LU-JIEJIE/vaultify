d62044af5d558000dddad0be742c1b655642bb99
Lu Jiejie <1587133476@qq.com>
2024-04-11 19:16:53 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Vault from './vault'
+
+const vault = new Vault({
+  projectName: 'sakana'
+})
---
+++ src/vault.ts
@@ -0,0 +1,9 @@
+interface Options {
+  projectName: string
+}
+
+export default class Vault {
+  constructor(options: Options) {
+
+  }
+}
